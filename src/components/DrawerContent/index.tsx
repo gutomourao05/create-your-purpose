28e0978a254e18a224e47c9cecd703b504420147
Guto <gutomourao05@gmail.com>
2024-07-16 18:44:06 -0300

    logout settings

--- src/components/DrawerContent/index.tsx
+++ src/components/DrawerContent/index.tsx
@@ -1,16 +1,29 @@
 import { DrawerContentComponentProps } from "@react-navigation/drawer"
-import { Text, Image } from "react-native"
+import { Text, Image, View, TouchableOpacity } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
-import { useUser } from "@clerk/clerk-expo"
+import { useAuth, useUser } from "@clerk/clerk-expo"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 const DrawerContent = (drawerProps: DrawerContentComponentProps) => {
     const { user } = useUser()
+    const { signOut } = useAuth()
     return (
         <SafeAreaView style={styles.container}>
-            <Image source={{ uri: user?.imageUrl }} style={styles.image} />
-            <Text style={styles.text}>{user?.fullName}</Text>
+            <View style={styles.imageContainer}>
+                <Image source={{ uri: user?.imageUrl }} style={styles.image} />
+            </View>
+            <View style={styles.bodyContent}>
+                <View style={styles.box}>
+                    <Ionicons name="person" size={26} />
+                    <Text style={styles.text}>{user?.fullName}</Text>
+                </View>
+            </View>
+            <TouchableOpacity style={styles.buttonLogout} activeOpacity={0.8} onPress={() => signOut()}>
+                <Ionicons name="exit" size={28} color={"#FFF"} />
+                <Text style={styles.buttonLogoutText}>Sair</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
--- src/components/DrawerContent/styles.ts
+++ src/components/DrawerContent/styles.ts
@@ -4,14 +4,48 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
+        padding: 24,
+    },
+    imageContainer: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 24,
     },
     image: {
         width: 150,
         height: 150,
         borderRadius: 100,
     },
+    bodyContent: {
+        width: "100%",
+        alignItems: "center",
+    },
+    box: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 3,
+        marginBottom: 12,
+    },
     text: {
-        fontSize: 16,
-        fontWeight: "bold",
+        fontSize: 20,
+    },
+    buttonLogout: {
+        backgroundColor: "#000",
+        width: "50%",
+        height: 50,
+        borderRadius: 6,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 8,
+        position: "absolute",
+        bottom: 34,
+    },
+    buttonLogoutText: {
+        color: "#FFF",
+        fontSize: 18,
+        fontWeight: "700",
     }
 })
